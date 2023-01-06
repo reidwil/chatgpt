3935546812a6d5db737f95a4bd7935ab487f8f6b
Reid Williams <reid.williams.-nd@disneystreaming.com>
2023-01-06 02:09:28 -0500

    adding download feature

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 import argparse
 import pathlib # for writing python functions into files
 import openai # for making the python functions
-
+import requests
+import random
 
 key = os.getenv('OPENAI_API_KEY') # get this on their website
 
@@ -11,7 +12,8 @@ def get_args():
     output = argparse.ArgumentParser(description="A python cli that allows you to ask and draw pictures upon giving openai's bots some topics.")
     output.add_argument('--ask', type=str, help="Text input of what you'd like to ask the bot")
     output.add_argument('--draw', type=str, help="Text input of what you'd like to see drawn")
-    output.add_argument('--output-file', type=os.PathLike, help="Location of std.out from program")
+    output.add_argument('-n', type=int, required=False, help="N times you want the bot to run - default is 2")
+    output.add_argument('--download', action='store_true', help="Should download the file?")
     output.parse_args()
     if len(sys.argv)==1:
         output.print_help(sys.stderr)
@@ -21,7 +23,7 @@ def get_args():
 class AI():
     def __init__(self, api_key):
         openai.api_key=api_key
-    
+
     def ask(self, prompt, model='text-davinci-003', temperature=0, max_tokens=600, top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0):
         response = openai.Completion.create(
             model=model,
@@ -35,7 +37,9 @@ class AI():
 
         return response
 
-    def draw(self, prompt, n = 2, size = "1024x1024"):
+    def draw(self, prompt, n = None, size = "1024x1024"):
+        if not n:
+            n = 2
         response = openai.Image.create(
             prompt=prompt,
             n=n,
@@ -43,7 +47,21 @@ class AI():
         )
 
         return response
-        
+
+    def generate_id(self):
+        return random.randint(1111,9999)
+
+    def parse_urls(self, draw_payload):
+        return [item["url"] for item in draw_payload["data"]]
+
+    def download(self, prompt, draw_payload):
+        print("Downloading your nicely made drawings")
+        urls = self.parse_urls(draw_payload)
+        for id, url in enumerate(urls):
+            img_data = requests.get(url).content
+            with open(f'{prompt}_{self.generate_id()}.jpg', 'wb') as handler:
+                handler.write(img_data)
+
 def run():
     bot = AI(key)
     arguments = get_args().parse_args()
@@ -51,8 +69,10 @@ def run():
         response = bot.ask(arguments.ask)
         print(response)
     if arguments.draw:
-        image_location = bot.draw(arguments.draw)
+        image_location = bot.draw(arguments.draw, n=arguments.n)
         print(image_location)
+    if arguments.download:
+        bot.download(arguments.draw, image_location)
 
 if __name__=='__main__':
     run()
